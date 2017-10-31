45ee31e7b8327fd7c1220ec74b382cee42297bcb
Haris Beha <harisbeha@gmail.com>
2017-10-30 23:56:08 -0400

    Create View index structure

---
+++ views/index.js
@@ -0,0 +1,5 @@
+export HomeView from './HomeView';
+export CheckInView from './CheckInView';
+export LandingView from './LandingView';
+export NewProductsView from './NewProductsView';
+export ReferFriendView from './ReferFriendView';
