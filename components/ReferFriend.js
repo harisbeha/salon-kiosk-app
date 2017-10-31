50dfd2dac2f9364431371bebdf0b81da7d29c841
Haris Beha <harisbeha@gmail.com>
2017-10-30 23:50:52 -0400

    Add Refer a Friend, Service components

---
+++ components/ReferFriend.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default class ReferFriend extends React.Component {
+  constructor(props, context) {
+    super(props);
+    context.router;
+    this.state = {};
+  }
+
+  render() {
+    return <div />;
+  }
+}
